127fdb3e438ff6c0af0fa203cd8f84034c36ff37
Kanayama-Take-sys <k.tennis.kinng@gmail.com>
2026-09-15 22:00:24 +0900

    あ

--- sutra copying/Ch2/Cbow.py
+++ sutra copying/Ch2/Cbow.py
@@ -1,7 +1,5 @@
 import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+sys.path.append('..')
 from common.layers import MatMul
 import numpy as np
 
@@ -21,7 +19,7 @@ out_layer = MatMul(W_out)
 # 順伝播
 h0 = in_layer0.forward(c0)
 h1 = in_layer1.forward(c1)
-h = 0.5 * (h0 + h1)
+h = 0.5 * (h0 + h1) # 二つの入力層を合計して重みを掛ける
 s = out_layer.forward(h)
 
 print(s)
